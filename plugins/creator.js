d14790ba0f2db33f2dc2dab528bb22ae4f8b85bc
XOrcistGans <112866322+XOrcistGans@users.noreply.github.com>
2022-09-21 11:45:21 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -21,7 +21,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
 ${htjava} *💌 Nama* : 𝐗𝐎𝐫𝐜𝐢𝐬𝐭𝐆𝐚𝐧𝐬
-${htjava} *✉️ Nama RL* : 𝐗𝐎𝐗
+${htjava} *✉️ Nama RL* : 𝐗𝐎𝐗𝐎
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Baru Lahir Kemaren 
